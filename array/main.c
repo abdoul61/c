dfffc2ac019287f2cf24b116d12ed64c4cc1b2ad
abdoul61 <souleyamine@gmail.com>
2025-07-04 17:55:55 -0400

    Added search optimization aka move-to-front, transposition

--- array/main.c
+++ array/main.c
@@ -43,15 +43,35 @@ void insert(struct Array *arr, int index, int x){
 int delete(struct Array * arr,int index){
     int el = arr->A[index];
     int i;
-    if(index < arr->length){
+    if(index >=0 && index < arr->length){
         for(i = index; i < arr->length-1; i++) {
             if(i+1 < arr->length){
                 arr->A[i] = arr->A[i+1];
             }
         }
+
+        arr->length--;
+        return el;
     }
-    arr->length--;
-    return el;
+    return 0;
+}
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+// Search can be optimized by using transposition or move-to-front
+int search(struct Array * arr,int x){
+    int i;
+    for(i = 0; i < arr->length; i++){
+        if (arr->A[i] == x){
+            swap(&arr->A[0],&arr->A[i]);
+            printf("Element is found!\n");
+            return i;
+        }
+    }
+    printf("Element is not present");
+    return -1;
 }
 
 int main(){
@@ -76,12 +96,18 @@ int main(){
     display(arr);
     append(arr,10);
     display(arr);
+    printf("Searching element\n");
+    search(arr,30);
     printf("The number of element in A is %d\n",arr->length);
     insert(arr,3,30);
     display(arr);
     printf("delete an element.\n");
     q = delete(arr,2);
     printf("Element deleted %d\n",q);
+    display(arr);
+    printf("Searching element\n");
+    printf("Element is found at %d\n",search(arr,30));
+    printf("Element is found at %d\n",search(arr,30));
     display(arr);
     free(arr->A);
     free(arr);
