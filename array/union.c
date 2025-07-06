7d61443f2836f6da1f265800d717f6b1fce74543
abdoul61 <souleyamine@gmail.com>
2025-07-06 14:38:54 -0400

    Added union function

---
+++ array/union.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Array{
+    int A[20];
+    int size;
+    int length;
+}
+
+void swap(int *a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+struct Array * union(struct Array *arr1, struct Array * arr2){
+    int i, j, k;
+    i = j = k = 0;
+    struct Array * result;
+    result = (struct Array *)malloc(sizeof(struct Array));
+
+    for(i)
+
+}
+int main(){
+    return 0;
+}
