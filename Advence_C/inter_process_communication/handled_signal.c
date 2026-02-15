248c66306914b0c4d66166072bc148125da7d64c
abdoul61 <Souleyamine@gmail.com>
2026-02-15 12:48:28 -1000

    Divided signal handled example

--- Advence_C/inter_process_communication/handled_signal.c
+++ Advence_C/inter_process_communication/handled_signal.c
@@ -1,17 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <signal.h>
 
 
+void handle_dividedByZero(int signum);
 int main(){
     int results = 0;
-    int v1 = 0, v2 = 0;
+    int v1 = 0;
+    int v2 = 0;
+    void (* sigHandlerReturn)(int);
+    sigHandlerReturn  = signal(SIGFPE,handle_dividedByZero);
+
+    if (sigHandlerReturn == SIG_ERR){
+        perror("Signal Error:");
+        return 1;
+    }
     v1 = 121;
     v2 = 0;
-    results = v1/v2;
+    results = (v1 / v2);
     printf("Result of Divide by zero is %d\n", results);
     return 0;
+}
+
+void handle_dividedByZero(int signum){
+    if (signum == SIGFPE) {
+        printf("Received %d signal \n", signum);
+
+    }else{
+        printf("Receice %d signal\n", signum);
+        return;
+    }
 }
 
 
